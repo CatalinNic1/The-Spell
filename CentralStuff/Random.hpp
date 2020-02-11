375aa7237e2e62ec48b6d0c4b2f07e23bcdda5ba
CatalinNic1 <catalinnicolau2013@gmail.com>
2020-02-11 22:00:18 +0200

    Random engine selection influenced by system bits (I don't have any idea how to write this)

--- CentralStuff/Random.hpp
+++ CentralStuff/Random.hpp
@@ -1,6 +1,8 @@
 #include <random>
 #include "TypeDefs.hpp"
 
+#if defined(__i386__)
+
 class RandomIntegral
 {
 private:
@@ -65,3 +67,72 @@ public:
         return randomDistribution(randomEngine); 
     }
 };
+
+#else
+
+class RandomIntegral
+{
+private:
+    //
+    static inline std::mt19937_64 randomEngine;
+public:
+    ///
+    ///
+    ///
+    static int getRandom(int Min, int Max)
+    {
+        std::uniform_int_distribution< int > randomDistribution(Min, Max);
+        return randomDistribution(randomEngine); 
+    }
+    ///
+    ///
+    ///
+    static int getRandom(int Max)
+    {
+        std::uniform_int_distribution< int > randomDistribution(0, Max);
+        return randomDistribution(randomEngine); 
+    }
+    ///
+    ///
+    ///
+    static int getRandom()
+    {
+        std::uniform_int_distribution< int > randomDistribution;
+        return randomDistribution(randomEngine); 
+    }
+};
+
+
+class RandomReal
+{
+private:
+    //
+    static inline std::mt19937_64 randomEngine;
+public:
+    ///
+    ///
+    ///
+    static float getRandom(float Min, float Max)
+    {
+        std::uniform_real_distribution< float > randomDistribution(Min, Max);
+        return randomDistribution(randomEngine); 
+    }
+    ///
+    ///
+    ///
+    static float getRandom(float Max)
+    {
+        std::uniform_real_distribution< float > randomDistribution(0.f, Max);
+        return randomDistribution(randomEngine); 
+    }
+    ///
+    ///
+    ///
+    static float getRandom()
+    {
+        std::uniform_real_distribution< float > randomDistribution;
+        return randomDistribution(randomEngine); 
+    }
+};
+
+#endif
