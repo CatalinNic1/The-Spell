3eeb6b54a6c05d422f24de233d312ca6da9920b1
CatalinNic1 <catalinnicolau2013@gmail.com>
2020-03-11 13:20:05 +0200

    Documentation!

--- CentralStuff/Random.hpp
+++ CentralStuff/Random.hpp
@@ -1,99 +1,6 @@
 #include <random>
 #include "TypeDefs.hpp"
 
-#if defined(__i386__)
-
-class RandomIntegral
-{
-private:
-    // Random generator engine (32-bit)
-    static inline std::mt19937 randomEngine;
-public:
-    ///
-    /// @brief Generates a number between a Min and a Max, so, a range-based generation
-    ///
-    /// @param Min - I don't know what to put here (I'm open to sugestions in regards)
-    ///
-    /// @param Max - Same as Min
-    ///
-    /// @return A random generated int
-    ///
-    static int getRandom(int Min, int Max)
-    {
-        std::uniform_int_distribution< int > randomDistribution(Min, Max);
-        return randomDistribution(randomEngine); 
-    }
-    ///
-    /// @brief Generates a number between 0 and Max, so, a range-based generation (I suggest to not put negative numbers)
-    ///
-    /// @param Max - I don't know what to put here (I'm open to sugestions in regards)
-    ///
-    /// @return A random generated int
-    ///
-    static int getRandom(int Max)
-    {
-        std::uniform_int_distribution< int > randomDistribution(0, Max);
-        return randomDistribution(randomEngine); 
-    }
-    ///
-    /// @brief Idk, generates a random number
-    ///
-    /// @return A random number between 0 and 2^32 - 1
-    ///
-    static int getRandom()
-    {
-        std::uniform_int_distribution< int > randomDistribution;
-        return randomDistribution(randomEngine); 
-    }
-};
-
-
-class RandomReal
-{
-private:
-    // Random generator engine (32-bit)
-    static inline std::mt19937 randomEngine;
-public:
-    ///
-    /// @brief Generates a number between a Min and a Max, so, a range-based generation
-    ///
-    /// @param Min - I don't know what to put here (I'm open to sugestions in regards)
-    ///
-    /// @param Max - Same as Min
-    ///
-    /// @return A random generated int
-    ///
-    static float getRandom(float Min, float Max)
-    {
-        std::uniform_real_distribution< float > randomDistribution(Min, Max);
-        return randomDistribution(randomEngine); 
-    }
-    ///
-    /// @brief Generates a number between 0 and Max, so, a range-based generation (I suggest to not put negative numbers)
-    ///
-    /// @param Max - I don't know what to put here (I'm open to sugestions in regards)
-    ///
-    /// @return A random generated int
-    ///
-    static float getRandom(float Max)
-    {
-        std::uniform_real_distribution< float > randomDistribution(0.f, Max);
-        return randomDistribution(randomEngine); 
-    }
-    ///
-    /// @brief Idk, generates a random number
-    ///
-    /// @return A random number between 0 and 1
-    ///
-    static float getRandom()
-    {
-        std::uniform_real_distribution< float > randomDistribution;
-        return randomDistribution(randomEngine); 
-    }
-};
-
-#else
-
 template< typename T >
 class RandomIntegral
 {
@@ -183,5 +90,3 @@ public:
         return randomDistribution(randomEngine); 
     }
 };
-
-#endif
--- Enemy/IntelligentEnemy/IntelligentEnemy.hpp
+++ Enemy/IntelligentEnemy/IntelligentEnemy.hpp
@@ -5,41 +5,59 @@
 class IntelligentEnemy : public Enemy
 {
 private:
-    //
+    // Used for eviting it
     sf::FloatRect Obstcale = sf::FloatRect();
-    //
-    static inline sf::Vector2f PlayerPos = SmartRect::Directions::Stop;
-    //
+    // Used to store the player position in order to follow it
+    static inline sf::Vector2f PlayerPos;
+    // Used to set a treshold value in order to stop following on one of axis
     static inline float EnemySpeed;
-    //
+    // The direction of the enemy in the mooment of collision
     sf::Vector2f DirectionOnCollision = SmartRect::Directions::Stop;
     ///
+    /// @brief Chooses the shorthest path according to the DirectionOnCollision variable
     ///
+    /// @return A predefined direction and a action to animate in case of collision with an obstcale
     ///
     std::pair< sf::Vector2f, Actions > EvitObstacle();
     ///
+    /// @brief Follows player and if it is colliding calls EvitObstcale()
     ///
+    /// @return A predefined direction and a action to animate
     ///
     virtual std::pair< sf::Vector2f, Actions > getAction() override;
     ///
+    /// @brief Checks if it is colliding with an obstcale
     ///
+    /// @return True if it does, False if it doesn't
     ///
     bool isColliding();
 public:
     ///
+    /// @brief Enemy constructor, this is where enemy will be initialized
     ///
+    /// @param Position - Where you want the enemy will be initially localized 
+    ///
+    /// @param Size - How big you want the enemy to be in pixel size
+    ///
+    /// @param TextureFileName - Path to the texture
     ///
     IntelligentEnemy(const sf::Vector2f& Position, const sf::Vector2f& Size, const std::string& TextureFileName);
     ///
+    /// @brief It occupies with moving, attacking, defending where is the case
     ///
+    /// @param DeltaTime - Time between frames
     ///
     virtual void Update(float DeltaTime) override;
     ///
+    /// @brief Sets the Obstcale varibale information such as: position and size
     ///
+    /// @param ObstcaleRect - The Globalbounds of the obstcale
     ///
     void setObstcaleInfo(const sf::FloatRect& ObstacaleRect);
     ///
+    /// @brief Sets where is the player across all instances
     ///
+    /// @param Destination - Player position
     ///
     static void setDestinationPos(const sf::Vector2f& Destination);
 };
